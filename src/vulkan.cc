71ca8f71ffd4d3722ac314c87ce7cd8ec7e71aaa
Russell Klenk <rklenk@us.ibm.com>
2018-10-15 12:53:49 -0700

    Add Vulkan loader library.

---
+++ src/vulkan.cc
@@ -0,0 +1,8 @@
+/**
+ * @summary vulkan.cc: Implements the Vulkan GPU back end.
+ * Depends on the Volk loader by Arseny Kapoulkine available at:
+ * https://github.com/zeux/volk.git
+ */
+#include "volk.h"
+#include "volk.c"
+
