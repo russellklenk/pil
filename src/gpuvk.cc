f7bf94b47e9874f751a3e0b858bec1bac278fa67
Russell Klenk <contact@russellklenk.com>
2019-01-17 10:57:57 -0800

    Update to VS2017 by default, remove Vulkan files.

--- src/gpuvk.cc
+++
@@ -1,9 +0,0 @@
-/**
- * @summary vulkan.cc: Implements the Vulkan GPU back end.
- * Depends on the Volk loader by Arseny Kapoulkine available at:
- * https://github.com/zeux/volk.git
- */
-#include "gpuvk.h"
-#include "volk.h"
-#include "volk.c"
-
